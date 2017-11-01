11061bc6a6628b03a317211bb5828f0e17414db6
Sushil <sushil.nayak@gmail.com>
2017-11-01 23:59:50 +0530

    cleanup

--- src/main/java/com/nayak/XmlApp.java
+++ src/main/java/com/nayak/XmlApp.java
@@ -122,27 +122,6 @@ public class XmlApp {
 
 	}
 	
-	  public static String formatXML(String input)
-	    {
-	        try
-	        {
-	            final InputSource src = new InputSource(new StringReader(input));
-	            final Node document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(src).getDocumentElement();
-
-	            final DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
-	            final DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
-	            final LSSerializer writer = impl.createLSSerializer();
-
-	            writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE);
-	            writer.getDomConfig().setParameter("xml-declaration", true);
-
-	            return writer.writeToString(document);
-	        } catch (Exception e)
-	        {
-	            e.printStackTrace();
-	            return input;
-	        }
-	    }
 }
 
 
